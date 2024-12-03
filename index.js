c22a6025888223b73cdb79aac7480709f7919c98
Kulam4ik <reimonzozuk123@gmail.com>
2024-12-03 16:51:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result); 
+
+
+const email = "reimonzozuk123@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+const My = "My";
+const name = "name";
+const is = "is";
+const fullName = My + name + is;
+const fulName = fullName + 'Viktor';
+console.log(fulName);
+
+
+const userName = "Олександр";
+const payment = 300;   
+const message = "Дякуємо, Олександро! До сплати 300 гривень";
+alert(message);
+
+
+
+
+
+
